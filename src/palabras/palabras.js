320cae68b4ce0e9d11e145975da0b8c45e2e163d
Augusto Paguaga <129023246+augustopaguaga@users.noreply.github.com>
2023-04-14 01:53:23 -0300

    Delete src directory

--- src/palabras/palabras.js
+++
@@ -1,5 +0,0 @@
-export const items = [
-    { id: "0", palabra1: "SALAME", palabra2: "CASINO", palabra3: "CALDO", palabra4: "VASO", palabra5: "COHETE",  },
-    { id: "1", palabra1: "SALAME", palabra2: "CASINO", palabra3: "CALDO", palabra4: "VASO", palabra5: "COHETE",  },
-
-]
